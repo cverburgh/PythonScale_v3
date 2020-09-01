64a214ca685d7d66fc0bba9329e88cd4e52c52ac
Craig Verburgh <craig@turtlebayit.com>
2020-09-01 12:22:03 -0700

    fixed typo

--- Test.py
+++ Test.py
@@ -13,7 +13,7 @@ lcd.screenOn();
 lcd.setText("Test Line 1", "Test Line 2", "Test Line 3", "Test Line 4")
 
 sleep(5)
-lcd.clearText();
+lcd.clearText()
 
 lcd.setText("Turning off LCD", "in", "   5   ","seconds")
 sleep(1)
@@ -26,10 +26,10 @@ sleep(1)
 lcd.setTextOnLine("    1    ", 3)
 sleep(1)
 lcd.setTextOnLine("    0    ", 3)
-lcd.screenOff();
+lcd.screenOff()
 	
 sleep(5)
-lcd.clearText();
+lcd.clearText()
 lcd.screenOn()
 
 lcd.setText("Clearing screen", "in", "   5   ","seconds")
@@ -43,10 +43,9 @@ sleep(1)
 lcd.setTextOnLine("    1    ", 3)
 sleep(1)
 lcd.setTextOnLine("    0    ", 3)
-lcd.clearText();
-	
+
 sleep(5)
-lcd.clearText();
+lcd.clearText()
 
 lcd.SetText("scroll up", "1", "2", "3")
 sleep(3)
@@ -59,9 +58,9 @@ sleep(0.5)
 lcd.addTextToBottom("7")
 	
 sleep(5)
-lcd.clearText();
+lcd.clearText()
 
-lcd.SetText("3", "2", "1", "scroll down")
+lcd.setText("3", "2", "1", "scroll down")
 sleep(3)
 lcd.addTextToTop("4")
 sleep(0.5)
@@ -72,9 +71,8 @@ sleep(0.5)
 lcd.addTextToTop("7")
 
 sleep(5)
-lcd.clearText();
+lcd.clearText()
 
-lcd.clearText();
 lcd.setText("powering off", "in 5 seconds")
 sleep(1)
 lcd.setTextOnLine("    4    ", 3)
