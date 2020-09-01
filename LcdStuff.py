5da182f0536cbf7c5e8e0cb8a2771173f0f3bb9e
Craig Verburgh <craig@turtlebayit.com>
2020-09-01 12:48:11 -0700

    test clear

--- LcdStuff.py
+++ LcdStuff.py
@@ -24,24 +24,24 @@ def setTextOnLine(text, line):
     lcdDisplay.set(text, line)
 
 def setText(line1 = "", line2 = "", line3 = "", line4 = ""):
-    lcdDisplay.set(line1, 1)
-    lcdDisplay.set(line2, 2)
-    lcdDisplay.set(line3, 3)
-    lcdDisplay.set(line4, 4)
+    lcdDisplay.set(fillLine(line1), 1)
+    lcdDisplay.set(fillLine(line2), 2)
+    lcdDisplay.set(fillLine(line3), 3)
+    lcdDisplay.set(fillLine(line4), 4)
 
 def addTextToTop(text):
     # add to top, lose the bottom
-    lcdDisplay.set(lcdDisplay.get(3), 4)
-    lcdDisplay.set(lcdDisplay.get(2), 3)
-    lcdDisplay.set(lcdDisplay.get(1), 2)
-    lcdDisplay.set(text, 1)
+    lcdDisplay.set(fillLine(lcdDisplay.get(3)), 4)
+    lcdDisplay.set(fillLine(lcdDisplay.get(2)), 3)
+    lcdDisplay.set(fillLine(lcdDisplay.get(1)), 2)
+    lcdDisplay.set(fillLine(text), 1)
 
 def addTextToBottom(text):
     # add to bottom, lose the first line
-    lcdDisplay.set(lcdDisplay.get(2), 1)
-    lcdDisplay.set(lcdDisplay.get(3), 2)
-    lcdDisplay.set(lcdDisplay.get(4), 3)
-    lcdDisplay.set(text, 4)
+    lcdDisplay.set(fillLine(lcdDisplay.get(2)), 1)
+    lcdDisplay.set(fillLine(lcdDisplay.get(3)), 2)
+    lcdDisplay.set(fillLine(lcdDisplay.get(4)), 3)
+    lcdDisplay.set(fillLine(text), 4)
 
 def fillLine(text):
     while (len(text) < 40):
